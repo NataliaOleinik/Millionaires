a1a8dd305d6ec7d1a4fea36a66e2b6690c60a682
Natalia Oleinik <olnataly81@gmail.com>
2020-06-10 15:32:32 +0700

    correct changes

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	var balance int32 = 1500000000
-	var transfer int32 = 1000000000
+	var balance int64 = 1500000000
+	var transfer int64 = 1000000000
 	total := balance + transfer
 	println(total)
 }
